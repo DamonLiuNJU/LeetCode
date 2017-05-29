ceb4f099ab21244edc4df72fcb1a913422e6be3d
DamonNJU <liu394000785@sina.com>
2017-05-29 21:49:27 +0800

    Q406 Accept
    我的做法是先排序，然后在做正常处理。
    但是总觉得可以通过直接一遍操作就能完成。
    应该是可以的，比如通过数组，然后放位置的时候规定已经放置好的位置不能向前移动。
    先排序的开销还是大了一些。

---
+++ src/com/Liuweiting/Q406QueueReconstructionbyHeight.java
@@ -0,0 +1,66 @@
+package com.Liuweiting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+
+/**
+ * https://leetcode.com/problems/queue-reconstruction-by-height/#/description
+ * Created by DamonLiu on 2017/5/29.
+ */
+public class Q406QueueReconstructionbyHeight {
+
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] - o2[1] != 0){
+                    return o1[1] - o2[1];
+                } else {
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+        ArrayList<int[]> list = new ArrayList<>();
+        for (int i = 0; i < people.length; i++) {
+            int[] tmp = people[i];
+            if (list.size()==0){
+                list.add(tmp);
+                continue;
+            }
+            int h = tmp[0];
+            int k = tmp[1];
+            int j;
+            for (j = 0; j < list.size() && k>0; j++) {
+                if (list.get(j)[0] >= h){
+                    k--;
+                }
+            }
+            if (k==0){
+                while (j<list.size() && list.get(j)[0]<h){
+                    j++;
+                }
+            }
+            list.add(j,tmp);
+        }
+        return list.toArray(new int[list.size()][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] input = {
+                {7,0},
+                {4,4},
+                {7,1},
+                {5,0},
+                {6,1},
+                {5,2}
+        };
+
+
+        input = new Q406QueueReconstructionbyHeight().reconstructQueue(input);
+        for (int[] tmp : input)
+            System.out.println(Arrays.toString(tmp));
+
+    }
+}
