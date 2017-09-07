a5d0055c43672ffdb7679e6290673ddadc7a8372
DESKTOP-A42T1RB\DamonLiu-Lab <liu394000785@sina.com>
2017-09-07 14:06:04 +0800

    101 questions solved, a small milestone.

---
+++ src/com/Liuweiting/DataStructure/ListNode.java
@@ -0,0 +1,13 @@
+package com.Liuweiting.DataStructure;
+
+/**
+ * Created by DamonLiu-Lab on 2017/9/7.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
